c3d6f569bbbe4c57f5f83030d0b7bf68c392a0c3
jayvyass <jayvyas08@yahoo.com>
2024-09-05 18:07:40 +0530

    first commit

---
+++ enhanced_image.py
@@ -0,0 +1,28 @@
+from PIL import Image
+import os
+
+def enhance_image(file_path):
+    # Open the image file
+    img = Image.open(file_path)
+
+    # Define new width and height
+    new_width = 3897
+    new_height = 4896
+    
+    # Resize the image
+    img = img.resize((new_width, new_height), Image.LANCZOS)
+    
+    # Define DPI
+    dpi = (400, 400)  
+    
+    # Save the image with the specified DPI
+    filename = 'enhanced_image.png'
+    img.save(filename, dpi=dpi)
+    
+    return filename
+
+# Example usage
+file_path = "/home/jay/Downloads/download.png" 
+enhanced_img = enhance_image(file_path)
+
+
